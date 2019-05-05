911c93258f9f9bf36ca6678320108a28e20ed8c4
teru07310731 <for.wind.to.blow.tomorrow@gmail.com>
2019-05-06 07:24:33 +0900

    04 cocmmit

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Folder;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -9,6 +10,10 @@ class TaskController extends Controller
     //
     public function index()
 	{
-		return "Hello world";
+		$folders = Folder::all();
+
+		return view('tasks/index', [
+			'folders' => $folders,
+		]);
 	}
 }
